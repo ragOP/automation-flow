7dd9cbb8b667532eaa85b13cf5cfd602da4fdae9
farish-jamal <farishjamal98@gmail.com>
2025-11-28 17:01:02 +0530

    refactor: update comment numbering for clarity in form submission handling

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -9,26 +9,27 @@ export const handleFormSubmit = async (req: Request, res: Response) => {
     try {
         const userData: UserFormData = req.body;
 
+        // 1. Save user to mongoDB to future use
+
         console.log("Received user data:", userData);
 
-        // 1. Call AI to get structured JSON
+        // 2. Call AI to get structured JSON
         const aiResponse = await generateAIResponse(userData);
         const cleaned = aiResponse.replace(/```json|```/g, "").trim();
 
         console.log("Cleaned AI Response:", cleaned);
         console.log("AI Response:", JSON.parse(cleaned));
 
-        // 2. Generate PDF based on AI JSON
+        // 3. Generate PDF based on AI JSON
         const pdfBuffer = await generatePDF(JSON.parse(cleaned));
 
-        // 3. Save PDF to a file (optional)
+        // 4. Save PDF to a file (optional)
         const downloadPath =  path.join(__dirname, "../downloads/horoscope.pdf");
         fs.writeFileSync(downloadPath, pdfBuffer);
 
         console.log(`PDF saved to ${downloadPath}`);
 
-
-        // 4. Return the PDF as a file download
+        // 5. Return the PDF as a file download
         res.setHeader("Content-Type", "application/pdf");
         res.setHeader("Content-Disposition", "attachment; filename=horoscope.pdf");
         
